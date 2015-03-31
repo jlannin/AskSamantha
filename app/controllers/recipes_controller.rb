a112418fe7b0251795aaf765a7152cea65e1b75c
sspano <sspano@colgate.edu>
2015-03-30 20:05:53 -0400

    update, new, create and show recipe controller methods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,11 +15,56 @@ def index
     end
 end
 
+def new
+  @recipe = Recipe.new #possibly replace this witha helper 
+end
+
+def create
+  r= Recipe.new(create_params)
+  if r.save
+    flash[:notice] = "New recipe #{r.name} was made"
+    redirect_to recipes_path
+  else
+    flash[:notice] = "The create didn't work :("
+    redirect_to new_product_path
+  end
+end
+
+
 def show
   @recipe = Recipe.find(params[:id])
 end
 
+#added simple edit metod to fix the null item error
+def edit 
+  @recipe = Recipe.find(params[:id])
+
+end
+
+def update
+  @recipe = Recipe.find(params[:id])
+  @recipe.update(update_params)
+  if @recipe.save#wont fail until we add validaitons or force in rspec tests
+    flash[:notice] = "You updated #{@recipe.name}"
+    redirect_to recipe_path(@recipe) #redirect to the show
+  else
+    flash[:notice] = "The update failed :("
+    redirect_to edit_recipe_path(@recipe) #redirect back to the edit page
+  end
+end
+
 private
+
+#added but still need to add the validations in the class
+def update_params
+  #mess with ingredients eventually
+  params.require(:recipe).permit(:name, :directions, :cooking_time, :image)
+end
+
+def create_params
+  params.require(:recipe).permit(:name, :directions, :cooking_time, :image)
+end
+
 
 def handle_redirects
   if !params.has_key?(:cooking_time) && session.has_key?(:cooking_time) && session[:cooking_time] != ""
