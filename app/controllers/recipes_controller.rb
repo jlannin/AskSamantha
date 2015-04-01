8790cd77ff795155b65ddab369f652b865fddcc5
jlannin <jlannin@colgate.edu>
2015-04-01 13:04:47 -0400

    Adding/Updating works with ingredients.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,15 +20,9 @@ def new
 end
 
 def create
-  #ingredients = params[:recipe].delete(:change_ingredients).split(",")
+  new_ingredients = params[:recipe].delete(:change_ingredients).split(",")
   r = Recipe.new(create_params)
-  #ingredients.each do |i|
-  #  i =~ /\s?(.*)\s(\d*)$/
-  #  hash = Hash.new
-  #  hash[:name] = $1
-  #  hash[:quantity] = $2
-  #  r.ingredients.new(hash)
-  #end
+  add_ingredients(new_ingredients, r, 0)
   if r.save
     flash[:notice] = "New recipe #{r.name} was made"
     redirect_to recipes_path
@@ -37,7 +31,6 @@ def create
     redirect_to new_recipe_path
   end
 end
-
 
 def show
   @recipe = Recipe.find(params[:id])
@@ -49,11 +42,15 @@ def edit
 end
 
 def update
-  #ingredients = params[:recipe].delete(:change_ingredients).split(",")
+  new_ingredients = params[:recipe].delete(:change_ingredients).split(",")
   @recipe = Recipe.find(params[:id])
   r = @recipe
+  index = update_ingredients(new_ingredients)
+  if new_ingredients.length > @recipe.ingredients.length
+    add_ingredients(new_ingredients, r, index)
+  end
   @recipe.update(update_params)
-  if @recipe.save#wont fail until we add validaitons or force in rspec tests
+  if @recipe.save#wont fail until we add validations or force in rspec tests
     flash[:notice] = "You updated #{@recipe.name}"
     redirect_to recipe_path(@recipe) #redirect to the show
   else
@@ -74,6 +71,33 @@ def create_params
   params.require(:recipe).permit(:name, :directions, :cooking_time, :image)
 end
 
+def add_ingredients(new_ingredients, r, start)
+  new_ingredients[start..new_ingredients.length-1].each do |i|
+    i =~ /\s?(.*)\s(\d*)$/
+    hash = Hash.new
+    hash[:name] = $1
+    hash[:quantity] = $2
+    r.ingredients.new(hash)
+  end
+end
+
+def update_ingredients(new_ingredients)
+  index = 0
+  old_ingredients = @recipe.ingredients
+  old_ingredients.each do |i|
+    if index < new_ingredients.length
+      new_ingredients[index] =~ /\s?(.*)\s(\d*)$/
+      hash = Hash.new
+      hash[:name] = $1
+      hash[:quantity] = $2
+      i.update(hash)
+    else
+      i.destroy
+    end
+    index += 1
+  end
+  index
+end
 
 def handle_redirects
   if !params.has_key?(:cooking_time) && session.has_key?(:cooking_time) && session[:cooking_time] != ""
